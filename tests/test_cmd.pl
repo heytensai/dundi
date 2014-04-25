7dc45d8cbe7a826d1e87f7b9935b79b8a9f56f4b
Corey Edwards <tensai@zmonkey.org>
2014-04-24 22:53:04 -0600

    pass in hex string on the command line

--- tests/test_cmd.pl
+++ tests/test_cmd.pl
@@ -8,7 +8,7 @@ use Dundi;
 
 my $dundi = Dundi->new();
 
-my $stream = 'aaaabbbb00000010140a00077470747074707470';
+my $stream = shift || 'aaaabbbb00000010140a00077470747074707470';
 
 $stream = pack('H*', $stream);
 
